7fa01f5c99f053cb03adfd4b01296940152bc2d9
Chance <jchancehud@gmail.com>
2023-04-17 00:28:12 -0500

    wip: goerli arb

--- goerli-arb/infra.tf
+++ goerli-arb/infra.tf
@@ -22,41 +22,39 @@ resource "aws_security_group" "goerli_arb" {
 }
 
 resource "aws_eip_association" "goerli_arb_assoc" {
-  instance_id = aws_instance.unirep_goerli_arb.id
+  instance_id = aws_instance.ebs_sync.id
   allocation_id = data.aws_eip.goerli_arb_eip.id
 }
 
-resource "aws_instance" "unirep_goerli_arb" {
-  ami = var.instance_ami
-  key_name = var.key_pair_name
-
-  instance_type = "i3.xlarge"
-
-  vpc_security_group_ids = [aws_security_group.goerli_arb.id]
-
-  root_block_device {
-    volume_size = 50
-  }
-
-  user_data = templatefile("./cloud-init.tftpl", {})
-
-  tags = {
-    Name = "unirep-goerli-arb"
-    Creator = "terraform"
-  }
+resource "aws_volume_attachment" "att" {
+  device_name = "/dev/sdz"
+  volume_id = "vol-08c1e72b78d2757c6"
+  instance_id = aws_instance.ebs_sync.id
 }
 
-resource "aws_instance" "ingress" {
+resource "aws_instance" "ebs_sync" {
   ami = var.instance_ami
   key_name = var.key_pair_name
+  availability_zone = "eu-west-3c"
 
-  instance_type = "t3.small"
+  instance_type = "c5.2xlarge"
 
   vpc_security_group_ids = [aws_security_group.goerli_arb.id]
 
   root_block_device {
     volume_size = 20
   }
+
+  /*
+  ebs_block_device {
+    device_name = "/dev/sdz"
+    snapshot_id = "snap-06444e1cea45ab166"
+    volume_type = "io2"
+    volume_size = 1000
+    iops = 50000
+  }
+*/
+  user_data = templatefile("./cloud-init-ebs.tftpl", {})
 
   tags = {
     Name = "unirep-goerli-ingress"
